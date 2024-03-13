f07255c36b9f8392080c66f856086550cebca826
Santosh_nc <santosh.nc26@gmail.com>
2024-03-14 03:26:52 +0530

    Initial commit AWS Resource Tracker Project

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#####################################
+#Author: Santosh Nc
+#Date: 14-March-2024
+#
+#Version: V1
+#
+#This script will report the AWS Usage
+#######################################
+#
+#Resources to Track:
+#  AWS S3
+#  AWS EC2
+#  AWS Lamda
+#  AWS IAM Users
+#
+
+echo "----------------------------------------------------------"
+echo "Listing the S3 Buckets"
+#list s3 buckets
+aws s3 ls
+
+echo "----------------------------------------------------------"
+echo "Listing the EC2 Instances"
+#list EC2 Instances
+aws ec2 describe-instances
+
+echo "----------------------------------------------------------"
+echo "Listing the AWS Lambda functions"
+#list AWS Lamda 
+aws lambda list-functions
+
+echo "----------------------------------------------------------"
+echo "Listing the IAM Users"
+#list IAM users
+aws iam list-users
